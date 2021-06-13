e5cd6a01608bd8f243139a655385a9cd2a675b7f
Pratush <pratush.bh@gmail.com>
2021-06-13 16:11:58 +0530

    Add PostListItem

---
+++ src/components/PostListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PostListItem = ({ id, title, description, createdAt }) => (
+    <Link className="list-item" to={`/edit/${id}`}>
+        <div>
+             <h3 className="list-item__title">{title}</h3>
+             <span className="list-item__sub-title">{description}</span>
+        </div>
+
+        <h3 className="list-item__data">{moment(createdAt).format('MMMM Do, YYYY')}</h3>
+    
+    </Link>
+)
+
+export default PostListItem;
