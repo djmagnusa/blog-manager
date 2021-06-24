ce8a1006e0186b1d5ca77c3810636c264cf91c2d
Pratush <pratush.bh@gmail.com>
2021-06-24 12:57:47 +0530

    Update DashboardHeader.js

--- src/components/DashboardHeader.js
+++ src/components/DashboardHeader.js
@@ -1,19 +1,62 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setTextFilter, sortByTitle, sortByDate } from '../actions/filters';
 
-const DashboardHeader = () => (
-    <div className="header-content">
-        <div className="filter-container">
-            <input className="text-input" placeholder="Search posts"></input>
-            <select className="select" name="filter">
-                <option value="byTitle">By Title</option>
-                <option value="byDate">By Date</option>
-            </select>
-        </div>
-        <div className="button-container">
-             <Link className="button" to="/create">Add Post</Link>
-        </div>
-    </div>
-)
+export class DashboardHeader extends React.Component{
 
-export default DashboardHeader;
+    onTextChange = (e) => {
+        this.props.setTextFilter(e.target.value);
+    }
+
+    onSortChange = (e) => {
+        if(e.target.value === 'title') {
+            this.props.sortByTitle();
+        } else if(e.target.value === 'date') {
+            this.props.sortByDate();
+        }
+    }
+ 
+    render() {
+
+        return (
+            <div className="header-content">
+                <div className="filter-container">
+                    <input 
+                        className="text-input" 
+                        placeholder="Search posts"
+                        value={this.props.filters.text}
+                        onChange={this.onTextChange}
+                    />
+                    <select 
+                        className="select" 
+                        value={this.props.filters.sortBy}
+                        onChange={this.onSortChange}
+                    >
+                        <option value="title">By Title</option>
+                        <option value="date">By Date</option>
+                    </select>
+                </div>
+                <div className="button-container">
+                    <Link className="button" to="/create">Add Post</Link>
+                </div>
+            </div>
+
+        )
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        filters: state.filters
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    setTextFilter: (text) => dispatch(setTextFilter(text)),
+    sortByTitle: () => dispatch(sortByTitle()),
+    sortByDate: () => dispatch(sortByDate())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardHeader);
