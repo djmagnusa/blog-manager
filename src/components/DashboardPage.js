5e6aae5a0b9f5aa738fe82c9a222e2580749fdfe
Pratush <pratush.bh@gmail.com>
2021-08-08 01:51:35 +0530

    Added BlogDetails component

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -3,9 +3,11 @@ import BlogDetails from './BlogDetails';
 import DashboardHeader from './DashboardHeader';
 import PostList from './PostList';
 
+//private page
+
 const DashboardPage = () => (
     <div>
-       <BlogDetails />
+       <BlogDetails /> 
        <DashboardHeader />
        <PostList />
     </div>
