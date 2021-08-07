1f8cb32eac0eb6a001d2bdcf84a0c8dad48d1690
Pratush <pratush.bh@gmail.com>
2021-08-08 01:50:21 +0530

    Update BlogDetails.js

---
+++ src/components/BlogDetails.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import selectPosts from '../selectors/posts';
+
+export const BlogDetails = ({ postCount }) => {
+
+    const postWord = postCount === 1 ? 'post' : 'posts';
+
+    return (
+        <div className="page-header">
+            <div className="content-container">
+                <div className="page-header__title">You currently have {postCount} active {postWord}</div>
+            </div>
+        </div>
+    );
+};
+
+
+const mapStateToProps = (state) =>{
+    const visiblePosts = selectPosts(state.posts, state.filters)
+
+
+    return {
+        postCount: visiblePosts.length
+    }
+}
+
+
+export default connect(mapStateToProps)(BlogDetails);
