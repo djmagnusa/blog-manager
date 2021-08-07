9a4155701f124649655e9ddf670650d0d5088ea9
Pratush <pratush.bh@gmail.com>
2021-08-08 01:49:07 +0530

    Update DashboardPage

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BlogDetails from './BlogDetails';
 import DashboardHeader from './DashboardHeader';
 import PostList from './PostList';
 
 const DashboardPage = () => (
     <div>
+       <BlogDetails />
        <DashboardHeader />
        <PostList />
     </div>
